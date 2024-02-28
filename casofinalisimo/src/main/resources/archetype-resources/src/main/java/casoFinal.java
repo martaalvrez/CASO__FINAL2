26d46f88c16c39a2f0ece6526ed7098282570712
martaalvrez <marta.alvarezarias@usp.ceu.es>
2024-02-28 09:37:03 +0100

    crear proyecto

---
+++ casofinalisimo/src/main/resources/archetype-resources/src/main/java/casoFinal.java
@@ -0,0 +1,13 @@
+package $org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class casoFinal
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
